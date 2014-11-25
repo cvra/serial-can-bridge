78a471e9a7bdce5a6d7c74c7f074613b5f54f8d2
Michael Spieler <the.chaotic.coder@gmail.com>
2014-11-25 22:30:33 +0100

    Update CAN frame packing test.

--- tests/can_frame_tests.cpp
+++ tests/can_frame_tests.cpp
@@ -17,21 +17,50 @@ TEST_GROUP(CANFrameTestGroup)
     }
 };
 
-TEST(CANFrameTestGroup, CanWriteFrame)
+TEST(CANFrameTestGroup, CanWriteStdFrame)
 {
-    bool ext;
-    uint32_t id;
-    uint8_t buf[8];
-    uint8_t len;
-    uint8_t out[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
-    bool res;
+    bool success;
+    struct can_bridge_frame write = {
+        .ext = 0,
+        .rtr = 0,
+        .dlc = 8,
+        .id.std = (1 << 11) - 1,
+        .data.u8 = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef}
+    };
+    struct can_bridge_frame read;
 
-    can_frame_cmp_write(&context, true, 42, out, sizeof(out));
+    can_frame_cmp_write(&context, &write);
 
-    res = can_frame_cmp_read(&context, &ext, &id, &buf, &len);
+    success = can_frame_cmp_read(&context, &read);
 
-    CHECK_TRUE(res);
-    CHECK_EQUAL(true, ext);
-    CHECK_EQUAL(42, id);
-    CHECK_EQUAL(0, memcmp(buf, out, sizeof(out)));
+    CHECK_TRUE(success);
+    CHECK_EQUAL(write.ext, read.ext);
+    CHECK_EQUAL(write.rtr, read.rtr);
+    CHECK_EQUAL(write.id.std, read.id.std);
+    CHECK_EQUAL(write.dlc, read.dlc);
+    CHECK_EQUAL(0, memcmp(write.data.u8, read.data.u8, write.dlc));
+}
+
+TEST(CANFrameTestGroup, CanWriteExtFrame)
+{
+    bool success;
+    struct can_bridge_frame write = {
+        .ext = 1,
+        .rtr = 0,
+        .dlc = 4,
+        .id.ext = CAN_FRAME_CMP_EXT_ID_MAX,
+        .data.u8 = "xkcd"
+    };
+    struct can_bridge_frame read;
+
+    can_frame_cmp_write(&context, &write);
+
+    success = can_frame_cmp_read(&context, &read);
+
+    CHECK_TRUE(success);
+    CHECK_EQUAL(write.ext, read.ext);
+    CHECK_EQUAL(write.rtr, read.rtr);
+    CHECK_EQUAL(write.id.ext, read.id.ext);
+    CHECK_EQUAL(write.dlc, read.dlc);
+    CHECK_EQUAL(0, memcmp(write.data.u8, read.data.u8, write.dlc));
 }
