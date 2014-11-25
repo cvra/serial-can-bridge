26390834a9bc64c6a39d57dc3e6eeddae019f808
Michael Spieler <the.chaotic.coder@gmail.com>
2014-11-25 22:29:18 +0100

    Simplify bridge code.

--- serial_can_bridge.c
+++ serial_can_bridge.c
@@ -1,30 +1,26 @@
 #include <serializer/serialization.h>
 #include <cmp.h>
-#include <serial-datagram/serial_datagram.h>
 #include "can_frame_cmp.h"
-#include "serial_interface.h"
 #include "can_interface.h"
 #include "serial_can_bridge.h"
 
-#define CMD_CAN_FRAME       0x00
-#define CMD_CFG_ID_FILTER   0x01
-
-
-bool can_send_cmp_frame(cmp_ctx_t *ctx)
+static bool can_send_cmp_frame(cmp_ctx_t *ctx)
 {
-    bool ext;
-    uint32_t id;
-    uint8_t data[8];
-    uint8_t len;
+    struct can_bridge_frame frame;
 
-    if (!can_frame_cmp_read(ctx, &ext, &id, data, &len)) {
+    if (!can_frame_cmp_read(ctx, &frame)) {
         return false;
     }
-    can_interface_send(ext, id, data, len);
+
+    if (frame.ext == 1) {
+        can_interface_send(true, frame.rtr == 1, frame.id.ext, frame.data.u8, frame.dlc);
+    } else {
+        can_interface_send(false, frame.rtr == 1, frame.id.std, frame.data.u8, frame.dlc);
+    }
     return true;
 }
 
-void serial_datagram_cb(const void *data, size_t len)
+void can_bridge_datagram_rcv_cb(const void *data, size_t len)
 {
     uint32_t cmd;
     serializer_t ser;
@@ -32,13 +28,14 @@ void serial_datagram_cb(const void *data, size_t len)
     serializer_init(&ser, (char *)data, len);
     serializer_cmp_ctx_factory(&ctx, &ser);
 
-    /* read command */
-    cmp_read_uint(&ctx, &cmd);
+    // read command
+    if (!cmp_read_uint(&ctx, &cmd)) {
+        return;
+    }
+
     switch (cmd) {
     case CMD_CAN_FRAME:
-        if (!can_send_cmp_frame(&ctx)) {
-            // todo: report error
-        }
+        can_send_cmp_frame(&ctx);
         break;
     case CMD_CFG_ID_FILTER:
         // todo
@@ -46,59 +43,20 @@ void serial_datagram_cb(const void *data, size_t len)
     }
 }
 
-void serial_rx_main(void* arg)
+bool can_bridge_frame_write(struct can_bridge_frame *frame, uint8_t *outbuf,
+    size_t *len)
 {
-    static char datagram_buf[1024];
-    static char serial_buf[32];
-    serial_datagram_rcv_handler_t rcv;
-
-    serial_datagram_rcv_handler_init(
-        &rcv,
-        datagram_buf,
-        sizeof(datagram_buf),
-        serial_datagram_cb);
-
-    while (1) {
-        int err;
-        size_t len;
-
-        /* listen to serial connection, blocking */
-        len = serial_interface_read(arg, serial_buf, sizeof(serial_buf));
-
-        /* parse for datagram, calls serial_datagram_cb for every datagram */
-        err = serial_datagram_receive(&rcv, serial_buf, len);
-        if (err != SERIAL_DATAGRAM_RCV_NO_ERROR) {
-            // todo: report error
-        }
-    }
-}
-
-void serial_tx_main(void *arg)
-{
-    static char outbuf[32];
-    bool ext;
-    uint32_t id;
-    uint8_t data[8];
-    uint8_t len;
-
     serializer_t ser;
     cmp_ctx_t ctx;
 
-    while (1) {
-        /* wait for can frame */
-        can_interface_receive(&ext, &id, &data, &len);
+    serializer_init(&ser, (char *) outbuf, *len);
+    serializer_cmp_ctx_factory(&ctx, &ser);
 
-        /* encode with MessagePack */
-        serializer_init(&ser, outbuf, sizeof(outbuf));
-        serializer_cmp_ctx_factory(&ctx, &ser);
-
-        if (!can_frame_cmp_write(&ctx, ext, id, data, len)) {
-            continue;
-        }
-
-        size_t outlen = serializer_written_bytes_count(&ser);
-        /* send within a serial datagram */
-        serial_datagram_send(outbuf, outlen, serial_interface_write, arg);
+    // encode with MessagePack
+    if (!can_frame_cmp_write(&ctx, frame)) {
+        return false;
     }
 
+    *len = serializer_written_bytes_count(&ser);
+    return true;
 }
--- serial_can_bridge.h
+++ serial_can_bridge.h
@@ -1,12 +1,33 @@
 #ifndef SERIAL_CAN_BRIDGE_H
 #define SERIAL_CAN_BRIDGE_H
 
+#include <stdint.h>
+#include <stddef.h>
+#include "can_frame_cmp.h"
+
+#define CMD_CAN_FRAME       0x00
+#define CMD_CFG_ID_FILTER   0x01
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void serial_rx_main(void *arg);
-void serial_tx_main(void *arg);
+/** Callback function for received datagrams
+ * @param [in] input input buffer.
+ * @param [in] len lenght of input buffer.
+ */
+void can_bridge_datagram_rcv_cb(const void *data, size_t len);
+
+
+/** Writes an encoded CAN Frame to a buffer
+ * @param [in] frame CAN frame to be encoded.
+ * @param [out] outbuf buffer to write to.
+ * @param [in] len length of outbuf.
+ * @param [out] len length of written bytes.
+ * @returns true if no error.
+ */
+bool can_bridge_frame_write(struct can_bridge_frame *frame, uint8_t *outbuf,
+    size_t *len);
 
 #ifdef __cplusplus
 }
