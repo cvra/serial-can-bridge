89e3549e4460aeab188bb3663e05b278db31a5e0
Michael Spieler <the.chaotic.coder@gmail.com>
2015-03-24 23:59:03 +0100

    Update CAN frame packing, change can_frame struct layout.

--- can_frame.h
+++ can_frame.h
@@ -10,17 +10,22 @@
 extern "C" {
 #endif
 
-#define CAN_FRAME_CMP_EXT_ID_MAX    ((1<<29) - 1)
-#define CAN_FRAME_CMP_STD_ID_MAX    ((1<<11) - 1)
+#define CAN_FRAME_STD_ID_MASK    ((1<<11) - 1)
+#define CAN_FRAME_EXT_ID_MASK    ((1<<29) - 1)
+#define CAN_FRAME_EXT_FLAG       (1<<29)
+#define CAN_FRAME_RTR_FLAG       (1<<30)
 
-struct can_bridge_frame {
-    uint8_t ext : 1;        // extended frame flag
-    uint8_t rtr : 1;        // remote transmission request
-    uint8_t dlc : 4;        // data length code
-    union {
-        uint32_t std : 11;
-        uint32_t ext : 29;
-    } id;                   // 11 bit or 29 bit extended ID
+/** id bit layout:
+ * [0-28]:  29 bit extended id
+ * ([0-10]:  11 bit basic id)
+ * [29]:    extended frame flag
+ * [30]:    remote transmission request flag
+ * [31]:    unused, set to 0
+ */
+
+struct can_frame {
+    uint32_t id;
+    uint8_t dlc;
     union {
         uint8_t u8[8];
         uint16_t u16[4];
@@ -33,14 +38,14 @@ struct can_bridge_frame {
  * @param [in] frame CAN frame to encode with MessagePack.
  * @returns true if no error.
  */
-bool can_frame_cmp_write(cmp_ctx_t *out, struct can_bridge_frame *frame);
+bool can_frame_cmp_write(cmp_ctx_t *out, struct can_frame *frame);
 
 /** Reads a MessagePack encoded CAN frame.
  * @param [in] MessagePack context input.
  * @param [out] frame CAN frame output.
  * @returns true if no error.
  */
-bool can_frame_cmp_read(cmp_ctx_t *in, struct can_bridge_frame *frame);
+bool can_frame_cmp_read(cmp_ctx_t *in, struct can_frame *frame);
 
 #ifdef __cplusplus
 }
