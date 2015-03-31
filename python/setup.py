3099d954763f12fad3675335c342b3228ffcbbde
Antoine Albertelli <antoinea101@gmail.com>
2015-03-31 13:41:15 +0200

    Extract python implementation from CAN bootloader

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+args = dict(
+    name='serial_can_bridge',
+    version='0.1',
+    description='Implements a MessagePack API for our CAN-UART bridge.',
+    packages=['can_bridge'],
+    author='CVRA',
+    author_email='info@cvra.ch',
+    url='https://github.com/cvra',
+    license='BSD'
+)
+
+setup(**args)
