3bf3512b6c976a68a442047da3ef931783f26606
Antoine Albertelli <antoinea101@gmail.com>
2015-04-01 15:14:31 +0200

    Add CAN ID filter setup command

--- python/can_bridge/commands.py
+++ python/can_bridge/commands.py
@@ -15,3 +15,24 @@ def encode_frame_write(frame):
     command = packer.pack(CANBridgeCommand.SendFrame)
     command = command + can_bridge.frame.encode(frame)
     return command
+
+def encode_id_filter_set(extended_frame=None, rtr_frame=None, id=0, id_mask=0):
+    """
+    Encodes a command to setup the CAN frame filter.
+
+    A None value for extended_frame and rtr_frame means that we don't care
+    about those values. For the id, a '1' bit in id_mask means that the filter
+    is enabled on that bit.
+    """
+    args = [False, False, id, False, False, id_mask]
+
+    if extended_frame is not None:
+        args[0], args[3] = extended_frame, True
+
+    if rtr_frame is not None:
+        args[1], args[4] = rtr_frame, True
+
+    args = msgpack.packb(args)
+
+    command = msgpack.packb(CANBridgeCommand.SetIDFilter)
+    return command + args
---
+++ python/tests/test_id_filter_command.py
@@ -0,0 +1,53 @@
+import unittest
+import can_bridge.commands
+import msgpack
+
+
+def unpackb(data):
+    unpacker = msgpack.Unpacker()
+    unpacker.feed(data)
+    return tuple(unpacker)
+
+
+class IdFilterTestCase(unittest.TestCase):
+    def test_encode_id_has_correct_command_number(self):
+        data = can_bridge.commands.encode_id_filter_set()
+        command = unpackb(data)[0]
+        self.assertEqual(1, command)
+
+    def test_default_values(self):
+        """
+        Checks that by default we don't apply any filter.
+        """
+        data = can_bridge.commands.encode_id_filter_set()
+        _, args = unpackb(data)
+        no_filter_args = [False, False, 0, False, False, 0]
+        self.assertEqual(args, no_filter_args)
+
+    def test_extended_flag(self):
+        """
+        Checks that applying the extended flag filter works.
+        """
+        data = can_bridge.commands.encode_id_filter_set(extended_frame=True)
+        _, args = unpackb(data)
+        expected_args = [True, False, 0, True, False, 0]
+        self.assertEqual(args, expected_args)
+
+    def test_rtr_flag(self):
+        """
+        Checks that applying the remote transmission request flag filter works.
+        """
+        data = can_bridge.commands.encode_id_filter_set(rtr_frame=True)
+        _, args = unpackb(data)
+        expected_args = [False, True, 0, False, True, 0]
+        self.assertEqual(args, expected_args)
+
+    def test_id_mask(self):
+        """
+        Checks that we can apply the ID filter mask.
+        """
+        data = can_bridge.commands.encode_id_filter_set(id=0x80, id_mask=0xff)
+        _, args = unpackb(data)
+        expected_args = [False, False, 0x80, False, False, 0xff]
+        self.assertEqual(args, expected_args)
+
